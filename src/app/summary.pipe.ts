0d1c912a6d3bc24e24502efe2d94710b6d387fd8
LostDakota <drewmika1@yahoo.com>
2018-12-02 21:33:56 -0500

    forgot to strip the html

--- src/app/summary.pipe.ts
+++ src/app/summary.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class Summary implements PipeTransform {
     transform(html: string, id: string, title: string): string {
         let maxLength = 256;
-        let sub = html.substring(0, maxLength);
+        let sub = html.replace(/<(?:.|\n)*?>/gm, '').substring(0, maxLength);
         return `${sub.substr(0, Math.min(sub.length, sub.lastIndexOf(" ")))}<a href="/post/${id}" title="Go to the post ${title}" class="color-link">... read more</a>`;
     }
 }
