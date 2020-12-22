66d04edcdf7dc89ab3626e03baa7ba150eec0aae
Drew Mika <drew.ubuntuserver@gmail.com>
2020-12-22 12:50:52 -0500

    conditionally serving webp vs jpg

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
+import { elementAt } from 'rxjs/operators';
 import { DescriptionService } from '../services/description.service';
 
 @Component({
@@ -8,7 +10,8 @@ import { DescriptionService } from '../services/description.service';
 
 export class AboutComponent {
   isVisible = false;
-  
+  imageUrl = "/assets/drew.jpg";
+
   constructor(
     private descriptionService: DescriptionService
   ) { };
@@ -16,8 +19,22 @@ export class AboutComponent {
   ngOnInit() {
     setTimeout(() => {
       this.isVisible = !this.isVisible;
-    }, 100);
-    
+    }, 100);    
+
+    if(this.checkBrowserSupport) {
+      this.imageUrl = "/assets/drew.webp";
+    }
+
     this.descriptionService.updateDescription('My name is Drew Mika and I am a full stack developer who is passionate about Javascript and front end. I work fulltime in Sitecore and C# at Hyland Software in Westlake, Ohio.')
+  }
+
+  checkBrowserSupport(){
+    const canvas = document.createElement('canvas');
+
+    if (!!(canvas.getContext && canvas.getContext('2d'))) {
+      return canvas.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    } else {
+      return false;
+    }
   }
 }
