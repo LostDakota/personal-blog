a59fcc0ffee8f0025ca2be3f84b36af4c2cfc4bd
LostDakota <drewmika1@yahoo.com>
2020-12-29 11:38:50 -0500

    removes bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,8 +4,7 @@ import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  templateUrl: './home.component.html'
 })
 
 export class HomeComponent implements OnInit {
