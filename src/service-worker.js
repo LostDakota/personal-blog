f5b22b1d9d6a8a946450d91c9af707f43cb31c27
LostDakota <drewmika1@yahoo.com>
2020-12-29 09:54:47 -0500

    removes the vendor chunk

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,10 +3,7 @@ var filesToCache = [
     'manifest.json',
     '/assets/android-icon-192x192.png',
     '/assets/favicon-16x16.png',
-    '/assets/favicon-96x96.png',
-    '//cdn.quilljs.com/1.3.6/quill.core.css',
-    '//cdn.quilljs.com/1.3.6/quill.snow.css',
-    '//cdn.quilljs.com/1.3.6/quill.bubble.css'
+    '/assets/favicon-96x96.png'
 ];
 
 self.addEventListener('install', function (e) {
